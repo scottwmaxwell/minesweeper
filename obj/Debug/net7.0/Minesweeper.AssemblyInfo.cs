f745b1f415a03c9cb0820a7d22a8eaa15741fd38
Miguel Zepeda <MZepeda13@my.gcu.edu>
2024-08-22 09:32:15 -0700

    Added Login views and some hardcoded users in the SecurityService.

--- obj/Debug/net7.0/Minesweeper.AssemblyInfo.cs
+++ obj/Debug/net7.0/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41ef599ceb806b31c1b276dddfcfb2b8306635a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52cf153d3359fc56d1090f96e68dc4252cada5d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
