93e567e1486edd6dcca4fe570e777420e50345fc
Miguel Zepeda <MZepeda13@my.gcu.edu>
2024-08-22 08:36:22 -0700

    Created user model.

--- obj/Debug/net7.0/Minesweeper.AssemblyInfo.cs
+++ obj/Debug/net7.0/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50a16ceccd67b5cc09540354dd3ee5cf43f26f00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76e871f132e9706fbc626040d4e3481bd9a311c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
