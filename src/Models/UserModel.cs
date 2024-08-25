51e0b6fc794545186e7eab0199784455f308eaf7
MoBBsKiLLz <migg_18@live.com>
2024-08-25 10:20:25 -0700

    Removed uncessary file and updated UserModel to have properties at the top.

--- src/Models/UserModel.cs
+++ src/Models/UserModel.cs
@@ -6,7 +6,44 @@ namespace Minesweeper.Models
 {
     public class UserModel
     {
+        public int Id { get; set; }
 
+        [DisplayName("First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [DisplayName("Sex")]
+        [Required(ErrorMessage = "Sex is required.")]
+        public string Sex { get; set; }
+
+        [DisplayName("Age")]
+        [Required]
+        [Range(1, 130)]
+        public int Age { get; set; }
+
+        [DisplayName("State")]
+        [Required]
+        [StringLength(13, MinimumLength = 4)]
+        public string State { get; set; }
+
+        [DisplayName("Email")]
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [DisplayName("Username")]
+        [Required]
+        [StringLength(15, MinimumLength = 4)]
+        public string UserName { get; set; }
+
+        [DisplayName("Password")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
 
         public UserModel()
         {
@@ -24,45 +61,6 @@ namespace Minesweeper.Models
             this.UserName = userName;
             this.Password = password;
         }
-
-        public int Id { get; set; }
-        
-        [DisplayName("First Name")]
-        [Required]
-        public string FirstName {  get; set; }
-        
-        [DisplayName("Last Name")]
-        [Required]
-        public string LastName { get; set; }
-        
-        [DisplayName("Sex")]
-        [Required(ErrorMessage = "Sex is required.")]
-        public string Sex { get; set; }
-        
-        [DisplayName("Age")]
-        [Required]
-        [Range(1, 130)]
-        public int Age { get; set; }
-        
-        [DisplayName("State")]
-        [Required]
-        [StringLength(13, MinimumLength = 4)]
-        public string State { get; set; }
-        
-        [DisplayName("Email")]
-        [Required]
-        [DataType(DataType.EmailAddress)]
-        public string Email {  get; set; }
-        
-        [DisplayName("Username")]
-        [Required]
-        [StringLength(15, MinimumLength = 4)]
-        public string UserName { get; set; }
-        
-        [DisplayName("Password")]
-        [Required]
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
     }
 
     public enum Gender
